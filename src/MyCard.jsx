8cc841e0f987b2278d05eca2eb4d73df84255318
conversx <jdxisuku0009@gmail.com>
2024-10-11 12:35:01 +0700

    ver 0.4 add card hover

--- src/MyCard.jsx
+++ src/MyCard.jsx
@@ -5,11 +5,19 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
 import { Button, CardActions } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const StyledCardMedia = styled(CardMedia)(({ theme }) => ({
+  transition: 'transform 0.3s ease',
+  '&:hover': {
+    transform: 'scale(1.1)',
+  },
+}));
 
 export default function MyCard(props) {
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
+      <StyledCardMedia
         component="img"
         height="140"
         image={props.attraction.coverimage}
@@ -30,6 +38,5 @@ export default function MyCard(props) {
         <Button size="small">Learn More</Button>
       </CardActions>
     </Card>
-
   );
 }
